936b054f25309e33f8b44937f0c63e026ffafbe8
OnlyNameUnused <32888916+OnlyNameUnused@users.noreply.github.com>
2018-12-26 16:41:55 -0500

    Refactored and starting adding logging functionality

--- index.js
+++ index.js
@@ -3,129 +3,24 @@
  *
  */
 
-// Dependencies
-var http = require('http');
-var https = require('https');
-var url = require('url');
-var StringDecoder = require('string_decoder').StringDecoder;
-var config = require('./lib/config');
-var fs = require('fs');
-var handlers = require('./lib/handlers');
-var helpers = require('./lib/helpers');
+ // Dependencies
+ var server = require('./lib/server');
+ var workers = require('./lib/server');
 
-// Testing Stripe api
-/* var stripeOptions = {
-  'amount' : 1000,
-  'currency' : 'usd',
-  'orderId' : 123456789,
-  'source' : 'tok_visa',
-  'description' : 'test submission',
-  'token' : config.stripeToken
-};
+ // Declare app
+ var app = {};
 
-helpers.processPayment(stripeOptions,function(res){
-  console.log('response: '+res);
-}) */
+ // Init function
+ app.init = function(){
+   // Start the server
+   server.init();
 
-// Instantiate the HTTP server
-var httpServer = http.createServer(function(req, res){
-  unifiedServer(req,res);
-});
+   // Start the workers
+   // workers.init();
+ };
 
-// Start the server
-httpServer.listen(config.httpPort,function(){
-  console.log("The server is listening on port "+config.httpPort);
-});
+ // Execute
+ app.init();
 
-// Instatiate the HTTPS server
-var httpsServerOptions = {
-  'key' : fs.readFileSync('./https/key.pem'),
-  'cert' : fs.readFileSync('./https/cert.pem')
-};
-var httpsServer = https.createServer(httpsServerOptions,function(req, res){
-  unifiedServer(req,res);
-});
-
-// Start the HTTPS server
-httpsServer.listen(config.httpsPort,function(){
-  console.log("The server is listening on port "+config.httpsPort);
-});
-
-
-// All the server logic for both http and https servervar
-var unifiedServer = function(req,res){
-
-  // Parse the url from the requests
-  var parsedUrl = url.parse(req.url,true);
-
-  // Get the path from the url and remove trailing /
-  var  path = parsedUrl.pathname;
-  var trimmedPath = path.replace(/^\/+|\/+$/g,'');
-
-	// Get query string from request as object
-	var queryStringObject = parsedUrl.query;
-
-  // Get request method
-  var method = req.method.toLowerCase();
-
-	// Get headers from request as object
-	var headers = req.headers;
-
-  // Get payload if any
-	var decoder = new StringDecoder('utf-8');
-	var buffer = '';
-	req.on('data',function(data){
-		buffer += decoder.write(data);
-	});
-	req.on('end',function(){
-		buffer += decoder.end();
-
-		// Choose the handler this request should use. If one is not found use the notFound handler
-		var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-
-		// Construct the data object to send to the handler
-		var data = {
-			'trimmedPath' : trimmedPath,
-			'queryStringObject' : queryStringObject,
-			'method' : method,
-			'headers' : headers,
-			'payload' : helpers.parseJsonToObject(buffer)
-		};
-
-		// Route the request to the specified route
-		chosenHandler(data,function(statusCode,payload){
-			// Use the status code called back or default to 200
-			statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-
-			// Use the payload called back or default to an empty object
-			payload = typeof(payload) == 'object' ? payload : {};
-
-			// Convert the payload to a string
-			var payloadString = JSON.stringify(payload);
-
-			// Return the response
-			res.setHeader('Content-Type','application/json');
-			res.writeHead(statusCode);
-			res.end(payloadString);
-
-			// Log request path to console
-			console.log('Returning this response: ',statusCode,payloadString);
-
-		});
-
-	});
-};
-
-// Define a request router
-var router = {
-	'ping' : handlers.ping,
-  'users' : handlers.users,
-  'tokens' : handlers.tokens,
-  'menu' : handlers.menu,
-  'orders' : handlers.orders,
-  'carts' : handlers.carts,
-  'carts/items/add' : handlers.carts,
-  'carts/items/update' : handlers.carts,
-  'carts/items/remove' : handlers.carts,
-  'carts/items/empty' : handlers.carts
-};
+ // Export the app
+ module.exports = app;
